1e0244784b4421e626259af0e97739e1bc990152
문서빈 <72489502+seobin-moon@users.noreply.github.com>
2023-11-28 08:07:59 +0900

    [level unrated] Title: 부분 문자열인지 확인하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
@@ -1,6 +1,5 @@
 def solution(my_string, target):
-    if target in my_string:
-        result = 1
-    else:
-        result = 0
-    return result
+    if target in my_string: return 1
+    else: return 0
+
+# return int(target in my_string) ?
