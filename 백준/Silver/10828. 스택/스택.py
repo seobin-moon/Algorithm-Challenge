4695e6d91c65b33bf19c9ca7a62ec4a2d99c3277
문서빈 <72489502+seobin-moon@users.noreply.github.com>
2024-02-01 16:54:31 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,26 +1,33 @@
 import sys
 n = int(sys.stdin.readline())
+# 한 줄 받을 때는 input을 사용해도 되지만, 
+# 반복문을 통해 입력받을 때는 input함수 사용 시 런타임 에러.
+# 입출력 속도 비교: sys.stdin.readline() > raw_input() > input
 
-stack=[]
+stack = []
 for i in range(n):
     command = sys.stdin.readline().split()
 
-    if command[0]=='push':
+    if command[0] == 'push':
         stack.append(command[1])
-    elif command[0]=='pop':
-        if len(stack)==0:
+
+    elif command[0] == 'pop':
+        if len(stack) == 0:
             print(-1)
         else:
             print(stack.pop())
+        
     elif command[0] == 'size':
         print(len(stack))
+
     elif command[0] == 'empty':
-        if len(stack)==0:
+        if len(stack) == 0:
             print(1)
         else:
             print(0)
+            
     elif command[0] == 'top':
-        if len(stack)==0:
+        if len(stack) == 0:
             print(-1)
         else:
             print(stack[-1])
