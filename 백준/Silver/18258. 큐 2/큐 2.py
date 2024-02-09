c381a57921ab635b48690af17e22f3b2afcd6e05
문서빈 <72489502+seobin-moon@users.noreply.github.com>
2024-02-09 12:54:19 +0900

    [Silver IV] Title: 큐 2, Time: 1656 ms, Memory: 173720 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,41 +1,67 @@
+class seobin_q:
+    def __init__(self):
+        self.q = [0]*2000000
+        self.sp = 0
+        self.ep = 0
+
+    def push(self, val):
+        self.q[self.ep] = val
+        self.ep += 1
+
+    def pop(self):
+        if self.sp == self.ep:
+            return -1
+        else:
+            tmp = self.q[self.sp]
+            self.sp += 1
+            return tmp
+        
+    def size(self):
+        return self.ep - self.sp
+    
+    def empty(self):
+        if self.ep == self.sp:
+            return 1
+        else:
+            return 0
+    
+    def front(self):
+        if self.ep == self.sp:
+            return -1
+        else:
+            return self.q[self.sp]
+        
+    def back(self):
+        if self.ep == self.sp:
+            return -1
+        else:
+            return self.q[self.ep - 1]
+
 import sys
 n = int(sys.stdin.readline())
 
-q = [0]*2000000
-sp = 0
-ep = 0
+sq = seobin_q()
 
-for i in range(n):
-    command = sys.stdin.readline().split()
+def execute_command(raw_command):
+    command = raw_command.split()
 
     if command[0] == 'push':
-        q[ep] = command[1]
-        ep += 1
+        sq.push(command[1])
 
     elif command[0] == 'pop':
-        if sp == ep:
-            print(-1)
-        else:
-            print(q[sp])
-            sp += 1
+        print(sq.pop())
         
     elif command[0] == 'size':
-        print(ep - sp)
+        print(sq.size())
 
     elif command[0] == 'empty':
-        if ep == sp:
-            print(1)
-        else:
-            print(0)
+        print(sq.empty())
             
     elif command[0] == 'front':
-        if ep == sp:
-            print(-1)
-        else:
-            print(q[sp])
+        print(sq.front())
     
     elif command[0] == 'back':
-        if ep == sp:
-            print(-1)
-        else:
-            print(q[ep - 1])
+        print(sq.back())
+
+for i in range(n):
+    execute_command(sys.stdin.readline())
