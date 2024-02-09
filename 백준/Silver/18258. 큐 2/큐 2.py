e624699242cdb025888f7c0f9b7155b2f3e350e2
문서빈 <72489502+seobin-moon@users.noreply.github.com>
2024-02-09 13:20:22 +0900

    [Silver IV] Title: 큐 2, Time: 1580 ms, Memory: 175200 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,67 +1,32 @@
-class seobin_q:
-    def __init__(self):
-        self.q = [0]*2000000
-        self.sp = 0
-        self.ep = 0
-
-    def push(self, val):
-        self.q[self.ep] = val
-        self.ep += 1
-
-    def pop(self):
-        if self.sp == self.ep:
-            return -1
-        else:
-            tmp = self.q[self.sp]
-            self.sp += 1
-            return tmp
-        
-    def size(self):
-        return self.ep - self.sp
-    
-    def empty(self):
-        if self.ep == self.sp:
-            return 1
-        else:
-            return 0
-    
-    def front(self):
-        if self.ep == self.sp:
-            return -1
-        else:
-            return self.q[self.sp]
-        
-    def back(self):
-        if self.ep == self.sp:
-            return -1
-        else:
-            return self.q[self.ep - 1]
-
 import sys
+from collections import deque
 n = int(sys.stdin.readline())
 
-sq = seobin_q()
+q = deque()
 
-def execute_command(raw_command):
-    command = raw_command.split()
+for _ in range(n):
+    command = sys.stdin.readline().split()
 
     if command[0] == 'push':
-        sq.push(command[1])
+        q.append(command[1])
 
     elif command[0] == 'pop':
-        print(sq.pop())
+        print(q.popleft()) if len(q) != 0 else print(-1)
         
     elif command[0] == 'size':
-        print(sq.size())
+        print(len(q))
 
     elif command[0] == 'empty':
-        print(sq.empty())
+        print(1 if len(q) == 0 else 0)
             
     elif command[0] == 'front':
-        print(sq.front())
+        if (len(q) == 0):
+            print(-1)
+        else:
+            print(q[0])
     
     elif command[0] == 'back':
-        print(sq.back())
-
-for i in range(n):
-    execute_command(sys.stdin.readline())
+        if (len(q) == 0):
+            print(-1)
+        else:
+            print(q[-1])
