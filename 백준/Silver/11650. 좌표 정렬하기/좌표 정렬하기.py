5e402b0fa0902751afe7b658aca63caa92b973ce
문서빈 <72489502+seobin-moon@users.noreply.github.com>
2024-03-05 08:55:23 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 4224 ms, Memory: 45364 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,8 +1,6 @@
-n = int(input())
-list = []
-for i in range(n):
-    [a, b] = map(int, input().split())
-    list.append([a, b])
-list.sort()
-for i in list:
-    print(i[0], i[1])
+N = int(input())
+# list는 앞으로 값이 변동할 수 있다는 것을 보여주고, tuple은 변동할 일이 없음을 보여줌.
+dots = [tuple(map(int, input().split())) for _ in range(N)] 
+dots = sorted(dots)
+for x, y in dots:
+	print(x, y)
