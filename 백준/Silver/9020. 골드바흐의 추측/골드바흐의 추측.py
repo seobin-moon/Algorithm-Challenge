425d206376137e61c762f69147e6bbdcc3b0ded7
문서빈 <72489502+seobin-moon@users.noreply.github.com>
2024-03-30 22:08:05 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 592 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,57 +1,24 @@
-# 소수 구하기 - 에레토스테네스의 체
+from math import sqrt
+
+# 에라토스테네스의 체
 def is_prime(num):
     if num == 1:
         return False
     else:
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
+        for i in range(2, int(sqrt(num))+1): # 또는 int(num**0.5)
+            if num % i == 0: 
+                return False # 소수 아님
+        return True # 소수 맞음 
 
 t = int(input())
 
 for _ in range(t): 
     n = int(input())
-    tmp = n // 2
+    tmp = n//2
     
     while tmp > 0:
         if is_prime(tmp):
-            if is_prime(n - tmp):
+            if is_prime(n-tmp): # (n - (n//2)) 값도 소수인지 확인
                 print(tmp, n-tmp)
                 break
         tmp -= 1
-
-
-
-
-# 소수인지 판별하는 알고리즘
-# from math import sqrt
-
-# def get divisiors(n):
-# 	s = set()
-# 	for i in range(1, int(sqrt(n)+1)):
-# 		if n % i == 0:
-# 			s.add(i)
-# 			s.add(n // i)
-# 	return s
-
-# def is_prime(n):
-# 	return (len(divisiors(n)) == 2)
-
-# print(is_prime(5))
-
-
-# 에라토스테네스의 체 알고리즘
-# import math
-
-# N = 120
-# is_prime = [True] * (N+1) # 처음엔 모두 true로 초기화
-# is_prime[1] = [False] # 1은 소수가 아니므로
-
-# for i in range(2, int(math.sqrt(N)) + 1):
-# 	if not is_prime[i]: continue
-# 	for j in range(2*i, N+1, i):
-# 		is_prime[j] = False
-
-# for i in range(1, N+1):
-# 	print(i, is_prime[i])
